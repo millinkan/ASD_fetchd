969ff4fa7243b8404a31c2afb3f8589d090235c0
millinkan <genmillinkan@gmail.com>
2025-03-25 11:58:27 +0100

    Merge remote-tracking branch 'origin/ASD_fetchd' into ASD_fetchd

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
 // index.js
-//import React from "react";
-//import ReactDOM from "react-dom/client";
-//import { BrowserRouter } from "react-router-dom";
-//import App from "./App";
-//import MapView from "./components/MapView/MapView";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import MapView from "./components/MapView/MapView";
 
-//const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(
-	//<React.StrictMode>
-		//<BrowserRouter>
-			//<App />
-		//</BrowserRouter>
-	//</React.StrictMode>
-//);
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
-
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
+
